2bcb007ba1ab6c0337e7fd378b297d5eb3b5d58e
Diego13pa <diego.pa13@hotmail.com>
2016-03-05 03:51:38 -0300

    Pagamento por Diego: Arcabouço do pagamento

---
+++ LoginBancoTeste/Controllers/PagamentoController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginBancoTeste.Controllers
+{
+    public class PagamentoController : Controller
+    {
+        // GET: Pagamento
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PgmtoConfirma()
+        {
+            return View();
+        }
+
+        public ActionResult PgmtoVencido()
+        {
+            return View();
+        }
+
+        public ActionResult PgmtoVerifica()
+        {
+            return View();
+        }
+
+        public ActionResult PgmtoVlrVenc()
+        {
+            return View();
+        }
+    }
+}
---
+++ LoginBancoTeste/Models/Pagamento.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace LoginBancoTeste.Models
+{
+    public class Pagamento
+    {
+        [Key]
+        public int id { get; set; }
+
+        public long cod_boleto { get; set; }
+        public double valor { get; set; }
+        public char status { get; set; }
+        public String descricao { get; set; }
+        public DateTime data_venc { get; set; }
+        public DateTime data_pagam { get; set; }
+        public DateTime data_realiz { get; set; }
+        public DateTime data_agendada { get; set; }
+    }
+}
